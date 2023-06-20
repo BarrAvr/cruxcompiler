1d18c2b23369d68d012024da07fe1779d89f902d
Barr Avrahamov <barravr@gmail.com>
2023-06-20 13:08:00 -0700

    commented out storeinst, loadinst, and callinst

--- src/main/java/crux/backend/CodeGen.java
+++ src/main/java/crux/backend/CodeGen.java
@@ -308,7 +308,6 @@ public final class CodeGen extends InstVisitor {
       value_str = "$" + String.valueOf(value);
     }else if (i.getSrcValue().getClass() == LocalVar.class){
       value_str = "" + i.getSrcValue();
-      value_str = "$1";
     }else{
       int value = (int) ((IntegerConstant) i.getSrcValue()).getValue();
       value_str = "$" + String.valueOf(value);
@@ -330,9 +329,9 @@ public final class CodeGen extends InstVisitor {
 
   public void visit(LoadInst i) {
     //todo
-    int dest_offset = getStackSlot(i.getDst()) * 8;
-    int src_adr_offset = getStackSlot(i.getSrcAddress()) * 8;
-    out.printCode("movq -" + src_adr_offset + "(%rbp), -" + dest_offset + "(%rbp)");
+//    int dest_offset = getStackSlot(i.getDst()) * 8;
+//    int src_adr_offset = getStackSlot(i.getSrcAddress()) * 8;
+//    out.printCode("movq -" + src_adr_offset + "(%rbp), -" + dest_offset + "(%rbp)");
   }
 
   public void visit(NopInst i) {
@@ -340,57 +339,57 @@ public final class CodeGen extends InstVisitor {
   }
 
   public void visit(StoreInst i) {
-    int offset_1 = getStackSlot(i.getSrcValue()) * 8;
-    int offset_2 = getStackSlot(i.getDestAddress()) * 8;
-    out.printCode("movq -" + offset_2 + "(%rbp), %r10");
-    out.printCode("movq -" + offset_1 + "(%rbp), %r11");
-    out.printCode("movq %r10, %r11");
+//    int offset_1 = getStackSlot(i.getSrcValue()) * 8;
+//    int offset_2 = getStackSlot(i.getDestAddress()) * 8;
+//    out.printCode("movq -" + offset_2 + "(%rbp), %r10");
+//    out.printCode("movq -" + offset_1 + "(%rbp), %r11");
+//    out.printCode("movq %r10, %r11");
   }
 
   public void visit(ReturnInst i) {
-    var offset = -8 *getStackSlot(i.getReturnValue());
-    out.printCode("movq " + offset + "(%rbp), %rax");
+    var offset = 8 *getStackSlot(i.getReturnValue());
+    out.printCode("movq -" + offset + "(%rbp), %rax");
     out.printCode("leave");
     out.printCode("ret");
   }
 
   public void visit(CallInst i) {
-    //pass arguments to callee using args registers
-    //generate assembly for call instruction
-    //move return value (if there is one) into return register
-
-    //step 1: Store parameters in designated registers or stack
-    Symbol callee = i.getCallee();
-    List<LocalVar> params = i.getParams();
-    int offset = 0;
-    for (int j = 0;j < params.size(); j++) {
-      offset = getStackSlot(params.get(j)) * 8;
-      switch (j) {
-        case 0:
-          out.printCode("movq -" + offset + "(%rbp), %rdi");
-          break;
-        case 1:
-          out.printCode("movq -" + offset + "(%rbp), %rsi");
-          break;
-        case 2:
-          out.printCode("movq -" + offset + "(%rbp), %rdx");
-          break;
-        case 3:
-          out.printCode("movq -" + offset + "(%rbp), %rcx");
-          break;
-        case 4:
-          out.printCode("movq -" + offset + "(%rbp), %r8");
-          break;
-        case 5:
-          out.printCode("movq -" + offset + "(%rbp), %r9");
-          break;
-        default:
-          break;
-      }
-    }
-    //step 2: Code for call instruction
-    String calleeName = callee.getName();
-    out.printCode("call " + calleeName);
+//    //pass arguments to callee using args registers
+//    //generate assembly for call instruction
+//    //move return value (if there is one) into return register
+//
+//    //step 1: Store parameters in designated registers or stack
+//    Symbol callee = i.getCallee();
+//    List<LocalVar> params = i.getParams();
+//    int offset = 0;
+//    for (int j = 0;j < params.size(); j++) {
+//      offset = getStackSlot(params.get(j)) * 8;
+//      switch (j) {
+//        case 0:
+//          out.printCode("movq -" + offset + "(%rbp), %rdi");
+//          break;
+//        case 1:
+//          out.printCode("movq -" + offset + "(%rbp), %rsi");
+//          break;
+//        case 2:
+//          out.printCode("movq -" + offset + "(%rbp), %rdx");
+//          break;
+//        case 3:
+//          out.printCode("movq -" + offset + "(%rbp), %rcx");
+//          break;
+//        case 4:
+//          out.printCode("movq -" + offset + "(%rbp), %r8");
+//          break;
+//        case 5:
+//          out.printCode("movq -" + offset + "(%rbp), %r9");
+//          break;
+//        default:
+//          break;
+//      }
+//    }
+//    //step 2: Code for call instruction
+//    String calleeName = callee.getName();
+//    out.printCode("call " + calleeName);
   }
 
   public void visit(UnaryNotInst i) {
