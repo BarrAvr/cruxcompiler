86c10b5d527300d6f2c9f090308b6f2b0d3e02e6
Barr Avrahamov <barravr@gmail.com>
2023-06-19 21:47:36 -0700

    fixed CopyInst visit

--- src/main/java/crux/backend/CodeGen.java
+++ src/main/java/crux/backend/CodeGen.java
@@ -225,7 +225,7 @@ public final class CodeGen extends InstVisitor {
   }
 
   public void visit(CopyInst i) {
-    int dest_offset = getStackSlot(i.getDstVar());
+    int dest_offset = getStackSlot(i.getDstVar()); //fixed
     out.printCode("movq $" + i.getSrcValue() + "%r10");
     out.printCode("movq %r10, -" + dest_offset + "(%rbp)");
   }
